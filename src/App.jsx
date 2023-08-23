67c6468381bb04b36d55ae03820b91c838b7c932
PhyuZinThant1997 <57340368+PhyuZinThant1997@users.noreply.github.com>
2023-08-23 11:44:17 +0630

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter  from './features/counter/Counter';
+
+const App = () => {
+  return (
+    <main>
+      <Counter />
+    </main>
+  )
+}
+
+export default App
